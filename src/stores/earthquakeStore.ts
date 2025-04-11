bf810aea9e8152e27639872dd7027673b478ea6c
Foram Patel <forampatel@forams-air.home>
2025-04-10 23:16:38 -0400

    refactor: removed highlightID and setHighlightID logic from zustand store

--- src/stores/earthquakeStore.ts
+++ src/stores/earthquakeStore.ts
@@ -3,15 +3,11 @@ import { Earthquake } from "../types/Earthquake";
 
 type EarthquakeStore = {
   data: Earthquake[];
-  highlightedId: string | null;
   setData: (data: Earthquake[]) => void;
-  setHighlightedId: (id: string | null) => void;
 };
 
 // create useEarthquakeStore to store all csv data(100 rows)
 export const useEarthquakeStore = create<EarthquakeStore>((set) => ({
   data: [],
-  highlightedId: null,
   setData: (data) => set({ data }),
-  setHighlightedId: (id) => set({ highlightedId: id }),
 }));
